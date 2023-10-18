3a1b0011ee3d4e3f3bd600f076a3f3ee3fafb046
Ryan Deida <rdeida27@gmail.com>
2023-10-17 20:16:36 -0400

    "initial commits for reviewme"

---
+++ src/views/Tabs/HomePage.vue
@@ -0,0 +1,16 @@
+<style scoped>
+</style>
+
+<template>
+    <ion-page>
+      <ion-searchbar animated="true" placeholder="Animated"></ion-searchbar>
+      <ion-content class="ion-padding">
+        <div class="Main-Menu"></div>
+      </ion-content>
+    </ion-page>
+  </template>
+
+<script setup lang="ts">
+ import { IonContent, IonPage } from '@ionic/vue';
+
+</script>
---
+++ src/views/Tabs/SearchPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <ion-page>
+        <ion-searchbar animated="true" placeholder="Animated"></ion-searchbar>
+        <ion-header>
+       
+      </ion-header>
+                <ion-tab-button tab="home" href="/home">
+                </ion-tab-button>
+ 
+      
+    </ion-page>
+</template>
+  
+<script setup lang="ts">
+import { IonPage, IonTabButton } from '@ionic/vue';
+
+</script>
